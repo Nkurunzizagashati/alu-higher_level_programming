c67c55f767972eb5c9c216cd49f57208ca0352bb
Fab <f.nkuruniz@alustudent.com>
2022-10-25 01:08:28 -0700

    a program that prints last digit of random numbers and print if they are greater than 5, if they are 0, or if they are less than 6 and ot 0

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    Last_digit = ((number * -1) % 10) * -1
+else:
+    Last_digit = number % 10
+if Last_digit > 5:
+    print('Last digit of {} is {} and is greater than 5' .format(number, Last_digit))
+elif Last_digit == 0:
+    print('Last digit of {} is {} and is 0' .format(number, Last_digit))
+elif Last_digit < 6 and Last_digit != 0:
+    print('Last digit of {} is {} and is less than 6 and not 0' .format(number, Last_digit))
