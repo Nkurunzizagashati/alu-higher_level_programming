6693e7c05e701e749f91591adc783d3cd4f5713a
Fab <f.nkuruniz@alustudent.com>
2022-10-26 05:10:41 -0700

    a program that checks for lower case letter without using islower() or isupper()

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in c:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            return True
-        else:
-            return False
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
