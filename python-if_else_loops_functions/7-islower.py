f880f475b39cd3d92b5cd89e2112dee5127fb371
Fab <f.nkuruniz@alustudent.com>
2022-10-26 05:04:24 -0700

    a program that prints true if the character in a word is lower case letter or false if a character is not lower case without using islower() or isupper()

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for i in c:
-        if ord(i) >= 97 and ord(i) <= 122:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
             return True
         else:
             return False
