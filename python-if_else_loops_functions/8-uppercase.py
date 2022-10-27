036e848e7bc07f764d96c785ed384d4639e97ee9
Fab <f.nkuruniz@alustudent.com>
2022-10-27 08:10:46 -0700

    a function which converts a string into uppercase letter without using str.upper()

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            f = chr(ord(i) - 32):
+            f = chr(ord(i) - 32)
         else:
             f = i
         print('{:s}'.format(f), end='')
