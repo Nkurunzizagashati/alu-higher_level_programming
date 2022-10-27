2183fe17f04dfd52371b8b4cb54312e1b8a4255b
Fab <f.nkuruniz@alustudent.com>
2022-10-27 08:59:26 -0700

    a function which remove character at a specified position

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    position = 0
+    new = ''
+    for i in str:
+        if position != n:
+            new = new + i
+        position = position + 1
+        return new
