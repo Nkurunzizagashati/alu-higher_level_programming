d560755c9f8763ac81a93e6886f2b8d1e3ce6ea5
Fab <f.nkuruniz@alustudent.com>
2022-10-27 00:53:16 -0700

    a function that prints Fizz for number which is multiple of 3, Buzz for numbers which are multiple of 5, and FizzBuzz for numbers which are multiples of both for numbers in range 1 - 101

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 100):
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
+        print('FizzBuzz', end=' ')
+    elif i == 100:
         print('Buzz')
+    elif i % 3 == 0:
+        print('Fizz', end=' ')
+    elif i % 5 == 0:
+        print('Buzz', end=' ')
     else:
-        print(i)
+        print(i, end=' ')
