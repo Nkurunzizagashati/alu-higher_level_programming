7bf86fbc21368fb3c6b9774ca8eed61f4bfaa779
Fab <f.nkuruniz@alustudent.com>
2022-11-01 02:14:27 -0700

    importing functions and using them to calculate the users input as a calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     number_of_arg = len(argv)
     if number_of_arg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
     op = argv[2]
@@ -19,5 +19,5 @@ if __name__ == "__main__":
         answer = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
     print("{:d} {} {:d} = {:d}".format(a, op, b, answer))
