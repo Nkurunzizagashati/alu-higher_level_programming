fccefdff0039d8f572b49c9302bf518586f1bafc
Fab <f.nkuruniz@alustudent.com>
2022-11-04 01:03:51 -0700

    removin some characters from as tring and print a new string without them

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = [i for i in my_string if i != "c" and i != "C"]
-    print("".join(new_str))
+    copy = [letter for letter in my_string if letter != 'c' and letter != 'C']
+    return ("".join(copy))
