59fc2cdf634aa096570fcdd5993c5a1dc63cf0d3
Fab <f.nkuruniz@alustudent.com>
2022-11-04 00:55:58 -0700

    removing some characters from a string to print a new_string without them

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = [i for i in my_string if i.upper != "C"]
+    new_str = [i for i in my_string if i.upper() != "C"]
     print("".join(new_str))
