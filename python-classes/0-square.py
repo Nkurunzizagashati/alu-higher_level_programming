db570c2aa70e477ddb0bd69f0efcd153509d9e6d
Fab <f.nkuruniz@alustudent.com>
2022-11-11 07:55:49 -0800

    defining an empty class

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#an empty class that defines a square
+"""
+   defining a class 'square'
+"""
+class square:
+    """
+      since it is an empty class we are going to use pass other wise if leave it empty it will give you an error.
+    """
+    pass
