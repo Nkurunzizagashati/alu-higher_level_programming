9be1339ca3664d0d1dcb0637c957081ac4dd6467
Fab <f.nkuruniz@alustudent.com>
2022-11-11 11:44:49 -0800

    a class Square that defines a square by: (based on 2-square.py) but at this time I will add a function that will print the area of the current square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,7 +23,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         '''
            a function that prints the area of the given square (current square)
