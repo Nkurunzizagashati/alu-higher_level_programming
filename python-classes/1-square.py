19c8a24904bc4b5a1c2c63917da29778a75f354f
Fab <f.nkuruniz@alustudent.com>
2022-11-11 08:49:55 -0800

    defing a class and pass come atributes to it

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# a class Square that defines a square by:
+# based on 0-square.py
+'''
+   a class that defines a square with a private instance attribute
+'''
+
+
+class Square:
+    '''
+       this is a class Square
+    '''
+    def __init__(self, size):
+        '''
+           this is an init method with its a tributes
+        '''
+        self.__size = size
