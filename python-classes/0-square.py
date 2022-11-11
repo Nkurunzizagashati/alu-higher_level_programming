5a2afcdf4d9343e48d09534a23bea63a1e6c05ba
Fab <f.nkuruniz@alustudent.com>
2022-11-11 08:02:33 -0800

    adding an empty class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,7 +4,7 @@
    defining a class 'square'
 """
 class square:
-"""
+    """
       since it is an empty class we are going to use pass other wise if leave it empty it will give you an error.
-"""
+    """
     pass
