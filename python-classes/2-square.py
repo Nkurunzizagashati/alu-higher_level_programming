ff88d7d62faab056e6bf18e3f50dc85b4c7e49f5
Fab <f.nkuruniz@alustudent.com>
2022-11-11 09:18:52 -0800

    a class Square that defines a square by: (based on 1-square.py)

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-# a class Square that defines a square by: (based on 1-square.py)
-# size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-#if size is less than 0, raise a ValueError exception with the message size must be >= 0
+# a class Square that defines a square by:
+# based on 1-square.py
+# size must be an integer, otherwise raise 
+# a TypeError exception with the message size must be an integer
+# if size is less than 0, raise a ValueError exception with the message size must be >= 0
 '''
    a class that defines a square
 '''
@@ -19,4 +21,4 @@ class Square:
             raise TypeError ("size must be an integer")
         elif size < 0:
             raise  ValueError ("size must be >= 0")
-        self.size = size
+        self.__size = size
