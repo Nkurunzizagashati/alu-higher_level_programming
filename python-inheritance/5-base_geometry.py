d007f68ab321f98db35afd2d40c0f8aaee1ff0be
Fab <f.nkuruniz@alustudent.com>
2022-11-17 04:00:13 -0800

    an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# an empty class BaseGeometry
+"""
+   define a base class 'BaseGeometry'
+"""
+
+
+class BaseGeometry:
+    """
+       this is an empty class
+    """
+    pass
