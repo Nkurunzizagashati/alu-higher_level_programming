c0c5344ea2bbb4ab4634913dfdba37302bd21555
Fabrice <f.nkurunziz@alustudent.com>
2022-11-25 12:21:29 -0800

    printing all inforrmations in the second table but only printing score and name and order by score in descending order

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+--  script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- but printing only two tables and order them by scores and in sescending order
+SELECT score, name FROM second_table ORDER BY score DESC;
