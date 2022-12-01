cc6a923ab1b82e0a98c96a0c379a21aa7334ac52
Fabrice <f.nkurunziz@alustudent.com>
2022-12-01 15:21:49 -0800

    a function that deletes keys with a specific value in a dictionary

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,5 +4,4 @@ def complex_delete(a_dictionary, value):
         if value in a_dictionary[i]:
             if a_dictionary[i] == value:
                 del a_dictionary[i]
-                break
-        return a_dictionary
+    return a_dictionary
