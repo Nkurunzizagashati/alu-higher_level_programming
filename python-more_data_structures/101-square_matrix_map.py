edb5dcf72473d67bc2b11d80a26e3fa7292742ba
Fabrice <f.nkurunziz@alustudent.com>
2022-12-01 14:54:49 -0800

    a function that computes the square value of all integers of a matrix using map

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    n_list = []
-    n_list.append(list(map(lambda x: x**2, my_list[0]))
-    n_list.append(list(map(lambda x: x**2, my_list[1]))
-    n_list.append(list(map(lambda x: x**2, my_list[2]))
-    return n_list
+    return list(map(lambda x: list(map(lambda n: n**2, x)), my_list))
