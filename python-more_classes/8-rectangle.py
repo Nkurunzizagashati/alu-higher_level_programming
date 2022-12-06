56d34a0fe045b72674ba272ceca4e68fbc62aa60
Fabrice <f.nkurunziz@alustudent.com>
2022-12-06 04:34:35 -0800

    this is similar to th e 7th question but for now we will add a method that will compare two instances(Rectangles)

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -104,7 +104,8 @@ class Rectangle:
         """
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
+    
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
             this method will return the biggest REctangle
