bb4b3664853f1fbb7918d030b1ef3c7e4ffe4382
Fabrice <f.nkurunziz@alustudent.com>
2022-12-14 07:05:32 -0800

    a class rectangle that inherits from Base class and it represents a rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,3 +28,64 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """
+            through this functtion we will get the value of width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, new_width):
+        """
+            the user will pass the new width value
+            in this function which will replace the
+            existing value
+        """
+        self.__width = new_width
+
+    @property
+    def height(self):
+        """
+            through this function, you can access the calue of height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, n_height):
+        """
+            this function will update the value of height
+        """
+        self.__height = n_height
+
+    @property
+    def x(self):
+        """
+            this function will read the position
+            on x_axis
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, new_x):
+        """
+            this function will update the position
+            on x_axis
+        """
+        self.__x = new_x
+
+    @property
+    def y(self, new_y):
+        """
+            this function will return the value of y
+            which is the position on y_axis
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, new_y):
+        """
+            this function updates the value of y
+        """
+        self.__y = new_y
