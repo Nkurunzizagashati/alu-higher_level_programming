c6a14a84e21c6af26eb91f0d7b12ce785af33bab
Fabrice <f.nkurunziz@alustudent.com>
2022-12-15 08:56:02 -0800

    square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -43,3 +43,37 @@ class Square(Rectangle):
             this is a special method
         """
         return "[Rectangle] ({}) {}/{} - {}".format(self.id, self.x, self.size)
+
+    def update(self, *args, **kwargs):
+        """
+            let use *args, and **qwargs
+        """
+        if args is not None and len(args) is not 0:
+            attributes = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if attributes[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, attributes[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+            this metho returns a dictionary
+            representation of a rectangle
+        """
+        attributes = ['id', 'size', 'x', 'y']
+        dict_repr = {}
+        for key in attributes:
+            if key == 'size':
+                dict_repr[key] = getattr(self, 'width')
+            else:
+                dict_repr[key] = getattr(self, key)
+        return dict_repr
