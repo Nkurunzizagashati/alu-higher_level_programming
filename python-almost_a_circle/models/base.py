257090748723f0859fdf0bc538ddd07fe11cd4cd
Fabrice <f.nkurunziz@alustudent.com>
2022-12-15 12:36:28 -0800

    updating our Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,27 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+            static method  that returns the JSON string representation
+            of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+            this method writes the JSON string representation
+            of list_objs to a file
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
