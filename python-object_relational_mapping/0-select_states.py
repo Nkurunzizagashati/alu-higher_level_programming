71d11273f50e66f863600e039a0b63af3b93fdba
Fabrice <f.nkurunziz@alustudent.com>
2022-12-16 00:50:34 -0800

    a script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    data_base = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], data_base=sys.argv[3])
-    a = data_base.cursor()
-    a.execute("SELECT * FROM 'states' WHERE BINARY 'name' == '{}'".format(sys.argv[4]))
-    [print(state) for state in a.fetchall()]
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM 'states' WHERE BINARY 'name' == '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
