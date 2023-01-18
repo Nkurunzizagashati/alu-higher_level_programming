e924c42b64de86d04d92a42114c28070dc11d404
Fabrice <f.nkurunziz@alustudent.com>
2023-01-18 14:45:32 -0800

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-# a script that lists all cities from the database hbtn_0e_4_usa
-# Your script should take 3 arguments: mysql username,
-# mysql password and database name
-# you must use the module MySQLdb (import MySQLdb)
-# Your script should connect to a
-# MySQL server running on localhost at port 3306
-# Results must be sorted in ascending order by cities.id
-# You can use only execute() once
-# Your code should not be executed when imported
 """
-    import 'MySQLdb' and 'sys' modules
+    A script that lists all cities from the database hbtn_0e_4_usa
 """
 
-import MySQLdb
+
 import sys
+import MySQLdb
 
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities JOIN states ON cities.state_id = states.id;")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        host="localhost",
+        port=3306
+    )
+    cursor = conn.cursor()
+
+    sql = """SELECT c.id, c.name, s.name
+          FROM states s, cities c
+          WHERE c.state_id = s.id
+          ORDER BY c.id ASC"""
+
+    cursor.execute(sql)
+    cities = cursor.fetchall()
+
+    for city in cities:
+        print(city)
+
+    cursor.close()
+    conn.close()
