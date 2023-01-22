defb609aa60baf2e3da5fd8eedbd7a1707bd2e62
Fabrice <f.nkurunziz@alustudent.com>
2023-01-22 02:23:37 -0800

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,88 +1,75 @@
 #!/usr/bin/python3
-"""Create a Square class for the work"""
-
-
+# a class Square that inherits from Rectangle
+# class
 from models.rectangle import Rectangle
+"""
+    define class 'Square'
+"""
 
 
 class Square(Rectangle):
-    """Create a Square class for the work"""
+    """
+        this is class Square that inherits Rectangle
+        class
+    """
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Create a Square class for the work
-
-        :param size: The size of the square
-        :param x: The x coordinate of the square
-        :param y: The y coordinate of the square
-        :param id: The id of the square
+            we will us super to inherit the attributes of the
+            Rectangle class.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """
-        Return the size of the square
-
-        :return: The size of the square
+            this method will help us to access the size
+            of the square
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-        Set the size of the square
-
-        :param value: The size of the square
+            we will call this method and pass a value we
+            want to give our size attribute
         """
-        self.width = value
         self.height = value
+        self.width = value
 
     def __str__(self):
         """
-        Return the string representation of the square
-
-        :return: The string representation of the square
+            this method will return
+            [Square] (<id>) <x>/<y> - <size>
         """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
-
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y,
+                                                 self.height)
     def update(self, *args, **kwargs):
         """
-        Update the square
-
-        :return: The updated square
+            the 1st arument must be id
+            2nd argument must be size
+            3rd armument must be x
+            4th argument must be y
+            if args is present kwargs will not be considered
+            else, its values will be assigned to Square attributes.
         """
-        if len(args) != 0:
-            try:
-                self.id = args[0]
-                self.size = args[1]
-                self.x = args[2]
-                self.y = args[3]
-            except IndexError:
-                pass
-
-        elif len(kwargs) != 0:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            else:
-                self.id
-
-            if "size" in kwargs:
-                self.size = kwargs["size"]
-            else:
-                self.size
-
-            if "x" in kwargs:
-                self.x = kwargs["x"]
-            else:
-                self.x
-
-            if "y" in kwargs:
-                self.y = kwargs["y"]
+        if args:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
 
     def to_dictionary(self):
         """
-        Return a dictionary representation of the square
-
-        :return: The dictionary representation of the square
+            this method will return the attributes of a square
+            in form of a dictionary, means with key value pair of the attributes.
         """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
