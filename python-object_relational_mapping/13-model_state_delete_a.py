f95e797363e6d6ba12cec3bb97e5193ceffa3afb
Nkurunzizagashati <f.nkurunziz@alustudent.com>
2023-01-23 07:33:21 -0800

    13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""script that deletes all State objects with a
-name containing the letter a from the database
-hbtn_0e_6_usa
+"""
+    script that deletes all State objects with a
+    name containing the letter a from the database
+    hbtn_0e_6_usa
 """
 
 import sys
