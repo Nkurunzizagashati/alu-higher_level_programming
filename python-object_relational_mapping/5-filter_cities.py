2a053a5b9379d546c65459447c3122df23d58b1c
Nkurunzizagashati <f.nkurunziz@alustudent.com>
2023-01-23 07:23:43 -0800

    7-model_state_fetch_all.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 '''
-    A script that takes in the name of a state as an
-    argument and lists all cities of that state, using
-    the database hbtn_0e_4_usa
+    this script takes in the name of
+    a state as an argument and lists all
+    cities of that state, using the database
+    hbtn_0e_4_usa
+    Your script should take
+    4 arguments: mysql username,
+    mysql password, database name and state
+    name (SQL injection free!)
+
+    You must use the module
+    MySQLdb (import MySQLdb)
+
+    Your script should connect to a MySQL server
+    running on localhost at port 3306
+
+    Results must be sorted in
+    ascending order by cities.id
+
+    You can use only execute() once
+
+    The results must be displayed as they are in
+    the example below
+
+    Your code should not be executed when imported
 '''
 
 
@@ -11,7 +32,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
@@ -21,19 +42,19 @@ if __name__ == "__main__":
 
     state_name = sys.argv[4]
 
-    cursor = conn.cursor()
+    cursor = db.cursor()
 
-    sql_com = """SELECT cities.name
+    command = """SELECT cities.name
           FROM states
           INNER JOIN cities ON states.id = cities.state_id
           WHERE states.name = %s
           ORDER BY cities.id ASC"""
 
-    cursor.execute(sql_com, (state_name,))
+    cursor.execute(command, (state_name,))
 
-    data = cursor.fetchall()
+    cities = cursor.fetchall()
 
-    print(", ".join([city[0] for city in data]))
+    print(", ".join([city[0] for city in cities]))
 
     cursor.close()
-    conn.close()
+    db.close()
