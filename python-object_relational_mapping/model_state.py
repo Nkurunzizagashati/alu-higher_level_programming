15ed9fd95d8b1e88612466717a41b570460e689c
Nkurunzizagashati <f.nkurunziz@alustudent.com>
2023-01-23 07:34:17 -0800

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,11 +2,11 @@
 # a python file that contains the class definition
 # of a State and an instance Base = declarative_base()
 """
-    importing functions from 'sqlalchemy' module
+    we will use sqlalchemy module in our codes
 """
 
 
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, String, MetaData, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = MetaData()
@@ -15,11 +15,8 @@ Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """
-        this class inherits from Base class
-        it has class attribute id that represents a column of
-        an auto-generated, unique integer, can’t be null and is a primary key
-        and a class attribute name that represents a column of a string
-        with maximum 128 characters and can’t be null
+        this is State class it inherits from Base class
+        and it can't be null
     """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
