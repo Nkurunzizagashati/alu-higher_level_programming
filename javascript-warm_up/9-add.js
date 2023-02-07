79beed9d6013b4428041c1b9d7fe378dd9c83453
Fab <f.nkurunziz@alustudent.com>
2023-02-07 09:29:35 -0800

    adding two arguments

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
-function add(a, b) {
+function add (a, b) {
   if (isNaN(Number(process.argv[2])) || isNaN(Number(process.argv[3]))) {
     console.log('NaN');
-} else {
-  console.log(a + b);
+  } else {
+    console.log(a + b);
   }
 }
 add(parseInt(a), parseInt(b));
