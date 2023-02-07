febfed2d71568764d8f63f9ab452d6e8af9551aa
Fab <f.nkurunziz@alustudent.com>
2023-02-07 08:35:08 -0800

    drawing a square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,8 +3,8 @@ let size = Number(process.argv[2]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let row = 'X'.repeat(size);
-  for (let i = 0; i < size; i += 1) {
+  const row = 'X'.repeat(size);
+  for (const i = 0; i < size; i += 1) {
     console.log(row);
   }
 }
