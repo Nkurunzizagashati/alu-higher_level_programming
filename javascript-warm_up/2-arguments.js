23d3194cba6a5e4f4a5bdcb50a509048be9b3fbe
Fab <f.nkurunziz@alustudent.com>
2023-02-07 01:51:58 -0800

    a script that counts arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 console.log(process.argv.length <= 2
-? console.log("No argument")
-: process.argv.length === 3
-? console.log("Argument found")
-: console.log("Arguments found"));
+  ? 'No argument'
+  : process.argv.length === 3
+  ? 'Argument found'
+    : 'Arguments found');
