4b58fd478abf1824d0188ca363c9fae15503c4b2
Fab <f.nkurunziz@alustudent.com>
2023-02-09 01:32:07 -0800

    swaping two the value of two properties

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const mySign = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i += 1) {
+      console.log(mySign);
+    }
+  }
+  rotate () {
+    let myVar = this.width;
+    this.width = this.height;
+    this.height = myVar;
+  }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
