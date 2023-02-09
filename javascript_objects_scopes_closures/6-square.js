dc1b1e2ffa9c6ee9fa170e830016c4a09c50ea0b
Fab <f.nkurunziz@alustudent.com>
2023-02-09 03:08:42 -0800

    6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,9 @@ module.exports = class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
   }
+
   charPrint (c) {
-    if (c != undefined) {
+    if (c !== undefined) {
       const mySign = c.repeat(this.width);
       for (let i = 0; i < this.height; i += 1) {
         console.log(mySign);
