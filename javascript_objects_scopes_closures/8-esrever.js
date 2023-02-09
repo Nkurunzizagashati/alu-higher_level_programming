46a22affacb9b3c1321894315d53fa44f7af3bbd
Fab <f.nkurunziz@alustudent.com>
2023-02-09 03:53:31 -0800

    returning a given list in a reversed order without using a builtin function reverse()

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let myNewList = [];
+  const myNewList = [];
   for (let i = list.length - 1; i >= 0; i -= 1) {
     myNewList.push(list[i]);
   }
   return myNewList;
-}
+};
