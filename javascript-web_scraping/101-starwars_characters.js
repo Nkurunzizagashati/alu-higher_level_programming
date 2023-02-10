ed3c1b9a660c55fad8277e2d9c4e82e47fb53d8c
Fab <f.nkurunziz@alustudent.com>
2023-02-10 13:14:59 -0800

    101-starwars_characters.js

---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request(url, (error, response, body) => {
+  if (!error) {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    characters.forEach(characterUrl, (err, response, body) => {
+      if (!err) {
+        const characterData = JSON.parse(body);
+        console.log(characterData.name);
+      }
+    });
+  }
+});
