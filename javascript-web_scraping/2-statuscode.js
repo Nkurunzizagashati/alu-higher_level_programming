f44882b1e418b7f4267c6b60be8204012a1c251c
Fab <f.nkurunziz@alustudent.com>
2023-02-10 02:48:26 -0800

    2-statuscode.js

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request.get(url, (response, error, data) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+request.get(url).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
