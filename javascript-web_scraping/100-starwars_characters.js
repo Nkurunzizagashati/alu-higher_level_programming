2ded79ead3cb1ae5a35832bc3c588e59852165cc
Fab <f.nkurunziz@alustudent.com>
2023-02-10 13:00:37 -0800

    100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -6,7 +6,7 @@ request.get(url, (error, response, body) => {
   if (!error) {
     const movie = JSON.parse(body);
     movie.characters.forEach(characterUrl => {
-      request.get(characterUrl, (error, response, body) => {
+      request.get(characterUrl, (err, response, body) => {
         if(!err) {
           const character = JSON.parse(body);
           console.log(character.name);
