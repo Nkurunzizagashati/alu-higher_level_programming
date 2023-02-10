b6be5b2e411b5c9fcf8bef230ae7bb9425f85d22
Fab <f.nkurunziz@alustudent.com>
2023-02-10 12:58:13 -0800

    100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -5,7 +5,7 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request.get(url, (error, response, body) => {
   if (!error) {
     const movie = JSON.parse(body);
-    movie.characters.forEach(characterurl => {
+    movie.characters.forEach(characterUrl => {
       request.get(characterUrl, (error, response, body) => {
         if(!err) {
           const character = JSON.parse(body);
