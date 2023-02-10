90c1d646c7781832cb0a36736d0ff792f209b64c
Fab <f.nkurunziz@alustudent.com>
2023-02-10 09:52:06 -0800

    6-completed_tasks.js

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const Tasks = {};
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const tasks = JSON.parse(body);
+  tasks.forEach(task => {
+    if (task.completed) {
+      if (!Tasks[task.userId]) {
+        Tasks[task.userId] = 0;
+      }
+      Tasks[task.userId]++;
+    }
+  });
+  Object.entries(Tasks).forEach(([userId, completedTasks]) => {
+    console.log(`User ID: ${userId}, Completed Tasks: ${completedTasks}`);
+  });
+});
