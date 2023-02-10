7032551bd22ad053d275c517c36b9f2c8481cc37
Fab <f.nkurunziz@alustudent.com>
2023-02-10 03:15:52 -0800

    3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const movieID = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-request.get(url, (response, error) => {
-  error
-  ? console.error(error)
-  : console.log(JSON.parse(response.body).title);
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request.get(url, (error, response) => {
+  error ? console.error(error) : console.log(JSON.parse(response.body).title);
 });
