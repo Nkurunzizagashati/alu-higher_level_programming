f311126d897fb12c73a75f4ce93a7dd0339ddcee
Fab <f.nkurunziz@alustudent.com>
2023-02-10 08:42:27 -0800

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@ request(url, function (error, response, body) {
   if (!error) {
     const movies = JSON.parse(body).results;
     console.log(movies.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endWith('/18/'))
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
